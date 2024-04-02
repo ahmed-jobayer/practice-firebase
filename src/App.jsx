97896e82dc5d501fa1d245d89d397d6f40b548f0
ahmed-jobayer <jobayer320721@.com>
2024-04-02 14:59:47 +0600

    practice done

--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,78 @@
-import './App.css'
-import { GoogleAuthProvider, getAuth, signInWithPopup, signOut } from "firebase/auth";
-import app from './Firebase/firebase,config';
-import { useState } from 'react';
-
+import "./App.css";
+import {
+  GithubAuthProvider,
+  GoogleAuthProvider,
+  getAuth,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
+import app from "./Firebase/firebase.config";
+import { useState } from "react";
 
 const auth = getAuth(app);
 const googleProvider = new GoogleAuthProvider();
-
-
+const githubProvider = new GithubAuthProvider();
 
 function App() {
-
-  const [user, setUser] = useState(null)
+  const [user, setUser] = useState(null);
 
   const handleGoogleSignIn = () => {
     signInWithPopup(auth, googleProvider)
+      .then((result) => {
+        const loggedUser = result.user;
+        console.log(loggedUser);
+        setUser(loggedUser);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
-    .then(result =>{
-      const loggedUser = result.user
-      console.log(loggedUser)
-      setUser(loggedUser)
-    })
-    .catch(error =>{
-      console.log(error)
-    })
-  }
+  const handleGithubSignIn = () => {
+    signInWithPopup(auth, githubProvider)
+      .then((result) => {
+        const loggedUser = result.user;
+        console.log(loggedUser);
+        setUser(loggedUser);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
-  const handleGoogleSignOut = () =>{
+  const handleGoogleSignOut = () => {
     signOut(auth)
-    .then(result  => {
-      console.log(result)
-      setUser(null)
-    })
-    .catch(error =>{
-      console.log (error)
-    })
-  }
+      .then((result) => {
+        console.log(result);
+        setUser(null);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
   return (
     <>
-      
       <h1>Vite + Firebase</h1>
-       {
-        user ?  <button onClick={handleGoogleSignOut}>Sign Out</button> :
+      {user ? (
+        <button onClick={handleGoogleSignOut}>Sign Out</button>
+      ) : (
         <div>
-        <button onClick={handleGoogleSignIn}>Google Sign In</button>
-        <button onClick={handleGoogleSignIn}>Github Sign In</button>
+          <button onClick={handleGoogleSignIn}>Google Sign In</button>
+          <button onClick={handleGithubSignIn}>Github Sign In</button>
         </div>
-       }
-     {user && <div className="card">
-        <h2>Name: {user.displayName}</h2>
-        <p>Email: {user.email}</p>
-        <div>
-          <img src={user.photoURL} alt="" />
+      )}
+      {user && (
+        <div className="card">
+          <h2>Name: {user.displayName}</h2>
+          <p>Email: {user.email}</p>
+          <div>
+            <img src={user.photoURL} alt="" />
+          </div>
         </div>
-       </div>
-     }
-      
+      )}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
